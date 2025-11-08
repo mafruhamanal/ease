9ef590bcbfe4f7e5ea817b322ba3bcd88c6dffa0
Aria Orni <ariaorni@Arias-MacBook-Air.local>
2025-11-08 16:41:01 -0500

    route path introduced

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 //import React, { useState, useEffect } from "react";
 import LogoAnimation from "./firstscreen";
+import NavBar from "./navBar";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 const ShapeTracingApp = () => {
  
   return (
     <div>
+
+    <BrowserRouter>
+    <NavBar />
+    <div className= "pages"> 
+      <Routes>
+        {/*<Route path="/" element= {}/>*/}
+        <Route path="/Landing" element= {<LogoAnimation/>} />
+        {/*<Route path="/ShapeCreator" element= {}/>
+        <Route path="/Tracing" element= {}/>
+        <Route path="/Posture" element= {}/> */}
+      </Routes>
+    </div>
+    </BrowserRouter>
     <LogoAnimation/> 
+
     <div className="min-h-screen bg-gradient-to-br from-purple-900 via-blue-900 to-indigo-900">
       <div className="container mx-auto px-4 py-8">
         
